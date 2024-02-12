ec6817a090f38d906c9fbd3170b0f33e2680d976
ghaefner <guillaume-haefner@gmx.de>
2024-02-12 21:08:55 +0100

    Fixed db issue

--- cPOP/src/db.py
+++ cPOP/src/db.py
@@ -12,8 +12,6 @@ class Tag(Base):
     key = Column(String, primary_key=True)
     tag = Column(String)
     tag_group = Column(String)
-    tag_total = Column(Integer)
-    year_total = Column(Integer)
 
 class Measure(Base):
     __tablename__ = Columns.MEASURE
@@ -21,36 +19,42 @@ class Measure(Base):
     year = Column(Integer)
     number = Column(Integer)
     fraction = Column(Float)
+    year_total = Column(Integer)
+    tag_total = Column(Integer)
+    
+def recreate_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
 
 def check_key(df, columns=[Columns.YEAR,Columns.TAG]):
     key_values = df[columns].drop_duplicates()
     if len(key_values) != len(df):
         warnings.warn("Columns are not a unique key.")
     else:
-        print("Key check ok.")
+        print(f'Key check ok. Key columns are: {columns}')
 
 def write_db(df, db_path=PATH_TO_DB):
-
     check_key(df)
-
     engine = create_engine(db_path)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    print(f"Writing table: {Columns.TAG} to {PATH_TO_DB}")
-
     # Inserting data into tags table
-    columns_for_tags_table = [Columns.ID, Columns.TAG, Columns.TAG_GROUP]
     df[Columns.ID] = df[Columns.YEAR].astype(str) + '@@' + df[Columns.TAG]
-    df[[columns_for_tags_table]].to_sql(Columns.TAG, con=engine, if_exists='replace', index=False)
-
-    print(f"Writing table: {Columns.MEASURE} to {PATH_TO_DB}")
+    # tags_data = tags_data.rename(columns={Columns.TAG: Columns.TAG_GROUP})
+    tags_entries = [
+        Tag(key=row[Columns.ID], tag_group=row[Columns.TAG_GROUP], tag=row[Columns.TAG])
+        for _, row in df.iterrows()
+    ]
+    session.add_all(tags_entries)
 
     # Inserting data into measures table
-    columns_for_measure_table = [Columns.ID, Column.YEAR, Columns.COUNT, Columns.FRACTION, Columns.YEAR_COUNT, Columns.TAG_COUNT]
-    df[columns_for_measure_table].to_sql(Columns.MEASURE, con=engine, if_exists='replace', index=False)
-
+    measures_entries = [
+        Measure(key=row[Columns.ID], year=row[Columns.YEAR], number=row[Columns.COUNT], fraction=row[Columns.FRACTION], tag_total=row[Columns.TAG_COUNT], year_total=row[Columns.YEAR_COUNT] )
+        for _, row in df.iterrows()
+    ]
+    session.add_all(measures_entries)
 
     session.commit()
     session.close()
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from cPOP.src.etl import read_data, calc_stats
 from cPOP.src.tags import make_tag_groups
 from cPOP.src.plot import plot_animation, save_fig, plot_tag_fraction
-from cPOP.constants import Columns
-from cPOP.src.db import write_db
+from cPOP.constants import Columns, PATH_TO_DB
+from cPOP.src.db import write_db, recreate_tables
+from sqlalchemy import create_engine
 
 print("Read data.")
 df = read_data()
@@ -14,5 +15,6 @@ print("Plot.")
 #plot_tag_group_fraction_animated(df,output_path="Output_Animation.gif")
 #save_fig(fig=plot_tag_fraction(df))
 #plot_animation(df,output_path="Output_Animation.gif")
-
+engine = create_engine(PATH_TO_DB)
+#recreate_tables(engine)
 write_db(df)
